c57cac4349e4aefe26e5ad95db023831d765c608
Jacqueline <jcasalia@yahoo.com.br>
2025-05-08 13:15:09 -0300

    Header

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import "./Styles/Global.css"
+import "./styles/Global.css"
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
